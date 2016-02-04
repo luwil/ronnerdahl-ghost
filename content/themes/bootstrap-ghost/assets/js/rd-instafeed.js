fb79a05e6ae0eba54a71ef9ef4c3ad4000c31abe
Johan Mattsson <johanmattsson1@gmail.com>
2016-02-04 14:48:14 -0400

    Instafeed update

--- content/themes/bootstrap-ghost/assets/js/rd-instafeed.js
+++ content/themes/bootstrap-ghost/assets/js/rd-instafeed.js
@@ -15,8 +15,6 @@ var feed = new Instafeed({
     template: '<a href="{{link}}"><img src="{{image}}" /></a>"{{caption}}"',
     clientId: 'see doc',
     accessToken: 'see doc',
-    clientId: '8342f4050bab4dbda7de62571792086b',
-    accessToken: '2734436307.8342f40.db35609a13ec4eab9aae43217fdcfcef'
 });
 
 feed.run();
